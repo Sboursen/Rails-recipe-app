922843b07645c46701e087a170dcae7236302fea
sboursen <dev.sboursen@gmail.com>
2022-06-20 14:34:16 +0100

    Add name to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_111715) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_133045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end

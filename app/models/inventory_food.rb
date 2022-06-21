7f7de12a9e6077975c127103d011e9cd0cc4d300
sboursen <dev.sboursen@gmail.com>
2022-06-21 11:53:52 +0100

    Add many-to-many relationship between Food and Inventory through InventoryFood

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,3 +1,6 @@
 class InventoryFood < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  belongs_to :inventory
+  belongs_to :food
 end

0c8223fd10d3aae7ca7e5963ce3a98768d1eea9a
sboursen <dev.sboursen@gmail.com>
2022-06-21 11:32:24 +0100

    Add User has_many Inventories relationship

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,5 @@
 class Inventory < ApplicationRecord
   validates :name, presence: true, length: { maximum: 250 }
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_102016) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_102824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_102016) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
   create_table "inventory_foods", force: :cascade do |t|
@@ -71,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_102016) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "inventories", "users"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"

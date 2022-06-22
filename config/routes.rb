cca8a8aabbe68c59711a5cc20f6ec57423314573
sboursen <dev.sboursen@gmail.com>
2022-06-22 16:15:25 +0100

    Make the public_recipes root

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :foods, only: [:index, :show, :new, :create, :destroy]
   resources :recipes, only: [:index, :show, :create, :new, :destroy]
-  resources :public_recipes, only: [:index, :show, :create, :new, :destroy]
-  root "foods#index"
+  resources :public_recipes, only: [:index]
+  root "public_recipes#index"
 end

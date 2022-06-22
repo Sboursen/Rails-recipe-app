92df8bda3dbe339d7852d36c9e728b5a3f2f476c
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-06-22 18:04:02 +0300

    skipp authentication

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,4 +1,5 @@
 class PublicRecipesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     @public_recipe = Recipe.where(public: true).order('created_at DESC')
   end

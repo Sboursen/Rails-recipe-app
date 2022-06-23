cb85ddb3d93514557d3cfb9b37b3ed400f6b1202
sboursen <dev.sboursen@gmail.com>
2022-06-23 06:51:02 +0100

    Use the correct formula to display public recipe info

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,14 @@
 class PublicRecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
-    @public_recipe = Recipe.where(public: true).order('created_at DESC')
+    @public_recipe_data = []
+    public_recipes = Recipe.where(public: true).order('created_at DESC')
+    public_recipes.each do |recipe|
+      public_recipe_data_unit = {}
+      public_recipe_data_unit[:recipe] = recipe
+      public_recipe_data_unit[:food_count] = recipe.foods.length
+      public_recipe_data_unit[:total_price] = recipe.recipe_foods.reduce(0) { |total_price, recipe_food| total_price + recipe_food.food.price * recipe_food.quantity }
+      @public_recipe_data << public_recipe_data_unit
+    end
   end
 end
